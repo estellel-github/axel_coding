49026ef58c966c4665c39787d3fb9585fac103b4
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-05-24 18:44:51 +0200

    Sync day 12

---
+++ 20240524/day12_exercise_sort.js
@@ -0,0 +1,9 @@
+const students = ["Zana", "Bojana", "Bernadetta", "Daniela", "Francieli", "Mariem", "Estelle", "Regina", "Shenoa", "Tina"]
+
+students.sort()
+
+console.log(students)
+
+students.sort().reverse()
+
+console.log(students)
