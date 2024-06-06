f869171826fc69008ae75bb83b73c2627f21fa6d
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-06-06 14:22:14 +0200

    -PIZZ- Start contact form JS validation

--- _Projects/-PIZZ-/javascript/main.js
+++ _Projects/-PIZZ-/javascript/main.js
@@ -11,3 +11,30 @@ function toggleMenu() {
     menuButton.src = "images/menu_lines.png";
   }
 }
+
+/* <form class="contact-form" action="#" method="post">
+<div class="form-group">
+  <label for="name">Name:</label>
+  <input type="text" id="form-name-input" name="name" required>
+</div>
+<div class="form-group">
+  <label for="email">Email:</label>
+  <input type="email" id="form-email-input" name="email" required>
+</div>
+<div class="form-group">
+  <label for="subject">Subject:</label>
+  <input type="text" id="form-subject-input" name="subject" required>
+</div>
+<div class="form-group">
+  <label for="message">Message:</label>
+  <textarea id="message" name="form-message-input" rows="5" required></textarea>
+</div>
+<button type="form-submit-button">Send Message</button>
+</form> */
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+});
